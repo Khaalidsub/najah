24c178232a80528055e297a9f2fec9fee2f907b4
Khaalidsub <khaalidsubaan@gmail.com>
2019-10-21 20:25:19 +0800

    frontend & member router fixes

--- viewModel/UserDA.js
+++ viewModel/UserDA.js
@@ -4,25 +4,41 @@ const bcrypt = require('bcrypt');
 
 const registerMember = async (User) => {
 	await User.save();
-}
-const registerEmployee = async function(user){
-	try{
-		await user.save()
-	}catch(e){
-		 res.status(401).send('somthing went wrong')
+};
+const registerEmployee = async function(user) {
+	try {
+		await user.save();
+	} catch (e) {
+		res.status(401).send('somthing went wrong');
 	}
- 
- }
+};
+const updateMember = async (user) => {
+	try {
+		//await User.u
+		console.log(user);
 
- const searchUser = async (email)=>{
-	 try {
-		const member = await User.find({email: email})
-		if(!member){res.send("no member found")}else{
+		await User.findByIdAndUpdate(user.id, { phone: user.phone });
+	} catch (error) {
+		res.status(401).send('something went wrong');
+	}
+};
+
+const searchUser = async (email) => {
+	try {
+		const member = await User.find({ email: email });
+		if (!member) {
+			res.send('no member found');
+		} else {
 			res.send(member);
 		}
-	 } catch (e) {
-		 res.send(e)
-	 }
- }
+	} catch (e) {
+		res.send(e);
+	}
+};
 
-module.exports = { registerMember: registerMember, registerEmployee: registerEmployee, searchUser:searchUser };
+module.exports = {
+	registerMember: registerMember,
+	registerEmployee: registerEmployee,
+	updateMember: updateMember,
+	searchUser: searchUser
+};
